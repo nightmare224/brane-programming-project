816d16d8e3d1e4edc9070111a29248d43a32272c
Hsiang-Ling Tai <nicole1999224@gmail.com>
2023-05-21 17:46:32 +0200

    label encoder done in brane

--- brane-heart-disease/packages/preprocessing/preprocessing.py
+++ brane-heart-disease/packages/preprocessing/preprocessing.py
@@ -4,11 +4,18 @@ import os
 import sys
 import json
 import pandas as pd
+from typing import List
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OrdinalEncoder
 
-def label_encoding(df: pd.DataFrame, column_names: list[str]) -> pd.DataFrame:
+def label_encoding(df: pd.DataFrame, column_names: List[str]) -> pd.DataFrame:
+    for column in column_names:
+        df[column] = LabelEncoder().fit_transform(df[column])
     return df
 
-def ordinal_encoding(df: pd.DataFrame, column_names: str, column_category: list) -> pd.DataFrame:
+def ordinal_encoding(df: pd.DataFrame, column_names: str, columns_categories: list) -> pd.DataFrame:
+    
     return df
 
 def one_hot_encoding(df: pd.DataFrame, column_names: str) -> pd.DataFrame:
@@ -16,13 +23,17 @@ def one_hot_encoding(df: pd.DataFrame, column_names: str) -> pd.DataFrame:
 
 # The entrypoint of the script
 if __name__ == "__main__":
-    functions = {"one_hot_encoding": one_hot_encoding, "ordinal_encoding": ordinal_encoding}
+    functions = {
+        "label_encoding": label_encoding,
+        "ordinal_encoding": ordinal_encoding,
+        "one_hot_encoding": one_hot_encoding, 
+    }
     if len(sys.argv) != 2 or (sys.argv[1] not in functions.keys()):
         print(f"Usage: {sys.argv[0]} {list(functions.keys())}")
         exit(1)
 
     # load args from environment variable
-    column_name = json.loads(os.environ["COLUMN_NAME"])
+    column_names = json.loads(os.environ["COLUMN_NAMES"])
     filepath = json.loads(os.environ["FILEPATH"])
     # the intermidate result seems would pass the directory
     if os.path.isdir(filepath):
@@ -31,12 +42,16 @@ if __name__ == "__main__":
     
     result_filepath = "/result/result.csv"
     cmd = sys.argv[1]
-    if cmd == "one_hot_encoding":
-        result = functions[cmd](df, column_name)
+    if cmd == "label_encoding":
+        column_names = eval(column_names)
+        result = functions[cmd](df, column_names)
         result.to_csv(result_filepath, index = False)
     elif cmd == "ordinal_encoding":
-        result = functions[cmd](df, column_name, [])
+        result = functions[cmd](df, column_names, [])
         result.to_csv(result_filepath, index = False)
         # from glob import glob
         # filepath = glob(f"{str(filepath)}/*", recursive = True)
         # print(yaml.dump({"result": str(filepath)}))
+    elif cmd == "one_hot_encoding":
+        result = functions[cmd](df, column_names)
+        result.to_csv(result_filepath, index = False)
